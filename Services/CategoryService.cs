02e8b2b9614f5ac1e0be1c3f78f86d128071ae90
volamdp95 <caunguyen.221@gmail.com>
2025-07-09 22:49:43 +0700

    category

---
+++ Services/CategoryService.cs
@@ -0,0 +1,59 @@
+﻿using GoEASy.Models;
+using GoEASy.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GoEASy.Services
+{
+    public class CategoryService : GenericRepository<TourCategory>, ICategoryService
+    {
+        private readonly GoEasyContext _context;
+
+        public CategoryService(GoEasyContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TourCategory>> GetAllCategoriesAsync()
+        {
+            return await _context.TourCategories.ToListAsync();
+        }
+
+        public async Task<TourCategory?> GetCategoryByIdAsync(int id)
+        {
+            return await _context.TourCategories.FindAsync(id);
+        }
+
+        public async Task CreateCategoryAsync(TourCategory category)
+        {
+            await _context.TourCategories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategoryAsync(TourCategory category)
+        {
+            _context.TourCategories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategoryAsync(TourCategory category)
+        {
+            _context.TourCategories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ToggleStatusAsync(int id)
+        {
+            var category = await _context.TourCategories.FindAsync(id);
+            if (category != null)
+            {
+                category.Status = !(category.Status ?? true);
+                category.UpdatedAt = DateTime.Now;
+                _context.TourCategories.Update(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using GoEASy.Models;
+using GoEASy.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GoEASy.Services
+{
+    public interface ICategoryService : IGenericRepository<TourCategory>
+    {
+        Task<List<TourCategory>> GetAllCategoriesAsync();
+        Task<TourCategory?> GetCategoryByIdAsync(int id);
+        Task CreateCategoryAsync(TourCategory category);
+        Task UpdateCategoryAsync(TourCategory category);
+        Task DeleteCategoryAsync(TourCategory category);
+        Task ToggleStatusAsync(int id);
+    }
+}
