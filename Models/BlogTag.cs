0d41cb825a11605129026034071f5eabe5d22b01
Binni1001 <duongtuankietqt@gmail.com>
2025-07-10 22:37:36 +0700

    Update database models

---
+++ Models/BlogTag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoEASy.Models;
+
+public partial class BlogTag
+{
+    public int TagId { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Blog> Blogs { get; set; } = new List<Blog>();
+}
