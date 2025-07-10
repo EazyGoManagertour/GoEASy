e42d8928b6521ec473981bb0af1e3635274ee33d
Binni1001 <duongtuankietqt@gmail.com>
2025-07-10 22:37:36 +0700

    Update database models

---
+++ Models/BlogComment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoEASy.Models;
+
+public partial class BlogComment
+{
+    public int CommentId { get; set; }
+
+    public int BlogId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Message { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Blog Blog { get; set; } = null!;
+
+    public virtual User? User { get; set; }
+}
---
+++ Models/BlogDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoEASy.Models;
+
+public partial class BlogDetail
+{
+    public int BlogDetailId { get; set; }
+
+    public int BlogId { get; set; }
+
+    public string? Introduction { get; set; }
+
+    public string? MainImage { get; set; }
+
+    public string? Section1Title { get; set; }
+
+    public string? Section1Content { get; set; }
+
+    public string? Section2Title { get; set; }
+
+    public string? Section2Content { get; set; }
+
+    public string? Quote { get; set; }
+
+    public string? QuoteAuthor { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Blog Blog { get; set; } = null!;
+}
