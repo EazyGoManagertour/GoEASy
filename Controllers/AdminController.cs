59e8860b23d40d920285c4c0c5c2b30670296fd7
Anh Khoa <cadilurk@gmail.com>
2025-07-15 22:43:55 +0700

    admin page redirect

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoEASy.Controllers
+{
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "LoginAdmin");
+        }
+    }
+}
