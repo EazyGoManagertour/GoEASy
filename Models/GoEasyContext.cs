d4f107b0884c248a696636607a7efb0f58887c8f
LeMinhNhat <leminhnhat11a2@gmail.com>
2025-07-16 13:27:48 +0700

    user,notification,bla bla

--- Models/GoEasyContext.cs
+++ Models/GoEasyContext.cs
@@ -67,6 +67,8 @@ public partial class GoEasyContext : DbContext
 
     public virtual DbSet<TourFAQ> TourFAQs { get; set; }
 
+    public virtual DbSet<Notification> Notifications { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=GoEasy;User Id=sa;Password=123456;TrustServerCertificate=True;");
