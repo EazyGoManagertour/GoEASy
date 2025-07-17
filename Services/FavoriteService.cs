7c87108c3ffad9edc3101372b49ea6f333b41fa1
Binni1001 <duongtuankietqt@gmail.com>
2025-07-17 10:31:46 +0700

    Lưu thay đổi trước khi chuyển sang nhánh favorite

--- Services/FavoriteService.cs
+++ Services/FavoriteService.cs
@@ -20,5 +20,45 @@ namespace GoEASy.Services
                 .Include(f => f.User)
                 .ToListAsync();
         }
+
+        public async Task<IEnumerable<Favorite>> GetFavoritesByUserIdAsync(int userId)
+        {
+            return await _context.Favorites
+                .Include(f => f.Tour)
+                .Include(f => f.User)
+                .Where(f => f.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<bool> RemoveFavoriteAsync(int userId, int tourId)
+        {
+            var favorite = await _context.Favorites
+                .FirstOrDefaultAsync(f => f.UserId == userId && f.TourId == tourId);
+
+            if (favorite != null)
+            {
+                _context.Favorites.Remove(favorite);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+        public async Task<bool> AddFavoriteAsync(int userId, int tourId)
+        {
+            var exists = await _context.Favorites.AnyAsync(f => f.UserId == userId && f.TourId == tourId);
+            if (exists)
+                return false;
+
+            var favorite = new Favorite
+            {
+                UserId = userId,
+                TourId = tourId,
+                CreatedAt = DateTime.Now
+            };
+            _context.Favorites.Add(favorite);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 } 
--- Services/IFavoriteService.cs
+++ Services/IFavoriteService.cs
@@ -7,5 +7,8 @@ namespace GoEASy.Services
     public interface IFavoriteService
     {
         Task<IEnumerable<Favorite>> GetAllFavoritesAsync();
+        Task<IEnumerable<Favorite>> GetFavoritesByUserIdAsync(int userId);
+        Task<bool> RemoveFavoriteAsync(int userId, int tourId);
+        Task<bool> AddFavoriteAsync(int userId, int tourId);
     }
 } 
